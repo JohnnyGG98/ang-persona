abb34e77a523813f28541afdb8464e006cabc3b7
Johnny <johnnygar98@hotmail.com>
2019-08-17 21:36:11 -0500

    Creando todos los componentes que usare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PersonComponent } from './components/person/person.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PersonComponent
   ],
   imports: [
     BrowserModule
